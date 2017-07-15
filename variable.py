9250deffd8e1070de10f372b5f8ccdea0e3b34e5
chaitanyagithub201296 <chaitu2012@gmail.com>
2017-07-15 11:44:49 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+num = raw_input('enter your number:');
+if num ==0:
+  print ('the number is zero');
+elif num >0:
+    print ('the number is positive');
+else:
+      print ('the number is negative');
